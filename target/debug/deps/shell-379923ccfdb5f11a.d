e3018df49b9f04ce49cd77cecea132a7570b4bf0
Ian Campbell <campbellni@hendrix.edu>
2023-04-23 13:41:48 -0500

    filesystem to lib, placed in src

---
+++ target/debug/deps/shell-379923ccfdb5f11a.d
@@ -0,0 +1,5 @@
+c:\Users\theia\Github\FileSystem\target\debug\deps\shell-379923ccfdb5f11a.rmeta: src\lib.rs
+
+c:\Users\theia\Github\FileSystem\target\debug\deps\shell-379923ccfdb5f11a.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/shell-60843e2ebf67390e.d
@@ -0,0 +1,5 @@
+c:\Users\theia\Github\FileSystem\target\debug\deps\shell-60843e2ebf67390e.rmeta: src\lib.rs
+
+c:\Users\theia\Github\FileSystem\target\debug\deps\shell-60843e2ebf67390e.d: src\lib.rs
+
+src\lib.rs:
